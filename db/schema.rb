a7f98ebeec1046665d2bf405023bc1c5868a310e
Yuanyi Zhang <zhangyuanyi@gmail.com>
2013-09-10 22:08:43 +0800

    mv active admin assets to vendor

--- db/schema.rb
+++ db/schema.rb
@@ -32,22 +32,6 @@ ActiveRecord::Schema.define do
 
   add_index :users, :email,                :unique => true
   add_index :users, :reset_password_token, :unique => true
-  
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.string   "queue"
-    t.datetime "created_at",                :null => false
-    t.datetime "updated_at",                :null => false
-  end
-
-  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table(:admin_users) do |t|
     ## Database authenticatable
